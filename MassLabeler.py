d8a2ec504fa50a579113ab93707990bb9087c738
schlafwandler <schlafwandler@darmstadt.ccc.de>
2020-06-19 19:41:36 +0200

    Handle malformed lines more gracefully

--- MassLabeler.py
+++ MassLabeler.py
@@ -1,5 +1,5 @@
 #
-#@author schlafwanlder
+#@author schlafwandler
 #@category 
 #@keybinding 
 #@menupath 
@@ -17,16 +17,23 @@ name_prefix = "p_"
 import ghidra.program.model.symbol.SourceType as SourceType
 
 def main():
-	symtab = currentProgram.getSymbolTable()
-	infile = str(askFile("Select input file","Input file"))
+    symtab = currentProgram.getSymbolTable()
+    infile = str(askFile("Select input file","Input file"))
 	
-	with open(infile,"r") as f:
-		for l in f.readlines():
-			addr,value,name = l.split()
+    with open(infile,"r") as f:
+        for l in f.readlines():
+            elements = l.split()
+            if len(elements) == 3:
+                addr,value,name = elements
+            elif len(elements) == 2:
+                addr,value = elements
+            else:
+                print("Malformed line '%s'"%(l))
+                continue
 			
-			address = currentAddress.getAddress(addr)
-			name = name_prefix+name
-			
-			symtab.createSymbol(address,name,SourceType.IMPORTED)
+            address = currentAddress.getAddress(addr)
+            name = name_prefix+name
+
+            symtab.createSymbol(address,name,SourceType.IMPORTED)
 			
 main()
